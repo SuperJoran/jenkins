c3142be17d1f9a95f304376b01ff5d1208cc0a3a
Joran De Boever <Joran.deboever@esfds.com>
2017-10-18 13:00:14 +0200

    qerhgqserh

---
+++ src/Config.groovy
@@ -0,0 +1,6 @@
+public class Config {
+
+    public static final String scmBranch = "master"
+    public static final String scmReleaseBranch = "${scmBranch}-release"
+    public static final String applicationConfig = "serviceplatform/r5"
+}
